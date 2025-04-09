bf225741e63f304d5e49520121fd5d0ea3b1ef5c
Santhosh Kumar <santhoshselvi.k01@gmail.com>
2025-04-09 15:54:20 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string EmployeeType { get; set; }
+
+        public Employee(int employeeId , string employeeName ,string employeeType)
+        {
+            EmployeeId = employeeId;
+            EmployeeName = employeeName;
+            EmployeeType = employeeType;
+
+        }
+
+        public void EmployeeSalaryDetails()
+        {
+            if(EmployeeType.ToUpper()=="BOSS")
+            {
+                Salary();
+            }
+            else
+            {
+                Console.WriteLine("You are not the boss!");
+            }
+        }
+
+        private void Salary()
+        {
+            Console.WriteLine("Salary:100000");
+        }
+    }
+}
